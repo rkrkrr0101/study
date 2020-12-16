c2b819b65183268cb1b83ceb3d4f3baddaf0d853
rkrkrr0101 <qkrrudgh0@naver.com>
2020-12-17 01:23:17 +0900

    1217

--- 리액트/newsviewer/src/compo/NewsList.js
+++ 리액트/newsviewer/src/compo/NewsList.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import styled from 'styled-components'
 import NewsItem from './NewsItem'
+import Axios from 'axios'
+import { useState } from 'react/cjs/react.development';
 
 const NewsListBlock=styled.div`
     box-sizing:border-box;
@@ -15,24 +17,40 @@ const NewsListBlock=styled.div`
     }
 
 `;
-const smapleArticle={
-    title:'제목',
-    description:'내용',
-    url:'https://google.com',
-    urlToImage:'https://via.placeholder.com/160',
-}
+
+
 
 
 const NewsList = () => {
+    const [articles,setArticles]= useState(null);
+    const [loading,setLoading]= useState(false);
+    useEffect(() => {
+        const fetchData= async()=>{
+            setLoading(true)
+            try{
+                const response= await Axios.get('http://newsapi.org/v2/top-headlines?country=kr&apiKey=3753f0a28bad4968a8532d43dafc7b33')
+                setArticles(response.data.articles)
+                
+            } catch(e){
+                console.log(e)
+            }
+            setLoading(false)
+        }
+        fetchData();
+    }, [])
+
+    if (loading){
+        return<NewsListBlock>대기중</NewsListBlock>
+    }
+    if(!articles){
+        return null
+    }
+
     return (
         <NewsListBlock>
-            <NewsItem article={smapleArticle}></NewsItem>
-            <NewsItem article={smapleArticle}></NewsItem>
-            <NewsItem article={smapleArticle}></NewsItem>
-            <NewsItem article={smapleArticle}></NewsItem>
-            <NewsItem article={smapleArticle}></NewsItem>
-            <NewsItem article={smapleArticle}></NewsItem>
-            <NewsItem article={smapleArticle}></NewsItem>
+            {articles.map(article=>(
+                <NewsItem key={articles.url} article={article}/>
+            ))}
         </NewsListBlock>
             
         

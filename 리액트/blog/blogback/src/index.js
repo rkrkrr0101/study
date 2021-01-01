39871f0a729ea013b1eead624b08a4ae1eb1ea05
rkrkrr0101 <qkrrudgh0@naver.com>
2021-01-01 21:15:14 +0900

    0101

--- 리액트/blog/blogback/src/index.js
+++ 리액트/blog/blogback/src/index.js
@@ -1,11 +1,15 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
+router.use('/api', api.routes());
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
-  console.log('listen to port 4000');
+    console.log('listen to port 4000');
 });

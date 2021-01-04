eb7f8e6896cee8060364be11d647806372a04049
rkrkrr0101 <qkrrudgh0@naver.com>
2021-01-04 23:02:04 +0900

    0104

---
+++ 리액트/blog/blogback/src/main.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+import createFakeData from './createFakeData';
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+    .then(() => {
+        console.log('connect');
+    })
+    .catch((e) => {
+        console.error(e);
+    });
+
+import api from './api';
+
+const app = new Koa();
+const router = new Router();
+
+router.use('/api', api.routes());
+
+app.use(bodyParser());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+console.log(PORT);
+const port = PORT || 4000;
+
+app.listen(port, () => {
+    console.log('listen to port %d', port);
+});

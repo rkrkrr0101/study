b63075b40691694c732b54d6e0bc3935268ee60e
rkrkrr0101 <qkrrudgh0@naver.com>
2021-01-07 12:01:37 +0900

    0107

---
+++ 리액트/blog/blogfront/src/pages/PostListPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '../compo/common/Button';
+
+const PostListPage = () => {
+    return (
+        <div>
+            <Button>버튼</Button>
+        </div>
+    );
+};
+
+export default PostListPage;

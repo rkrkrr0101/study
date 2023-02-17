d7e65a365c4b756644d21d19d1cd29575e3d5d26
foko <qkrrudgh0@naver.com>
2023-02-17 21:57:15 +0900

    0217

--- spring/exec/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ spring/exec/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +27,12 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder){
+        dataBinder.addValidators(itemValidator);
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -173,17 +182,21 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         log.info("object={}",bindingResult.getObjectName());
         log.info("target={}",bindingResult.getTarget());
 
-
+       // ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult,"itemName","required");
         if (!StringUtils.hasText(item.getItemName())){
             bindingResult.rejectValue("itemName","required");
 
+
         }
+
+
+
         if (item.getPrice()==null || item.getPrice()<1000 || item.getPrice()>1000000){
             bindingResult.rejectValue("price","range",
                                     new Object[]{1000,1000000},null);
@@ -214,8 +227,42 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
+    //@PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+       if( itemValidator.supports(item.getClass())){
+           itemValidator.validate(item,bindingResult);
+       }
 
 
+        if (bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
+
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+
+
+
+        if (bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
+
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);

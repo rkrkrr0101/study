b793fc413324f658cb83195e4753d13270404b15
foko <qkrrudgh0@naver.com>
2023-02-18 21:18:10 +0900

    0218

---
+++ spring/exec/validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,30 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    @Test
+    void beanValidation(){
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator=validatorFactory.getValidator();
+
+        Item item=new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> validates = validator.validate(item);
+        for (ConstraintViolation<Item> validate:validates){
+            System.out.println("validate = " + validate);
+            System.out.println("validate.getMessage() = " + validate.getMessage());
+        }
+    }
+}

793c3d082ae45a6dbd1d923ec6b0392cf9c954fc
foko <qkrrudgh0@naver.com>
2023-03-15 01:37:51 +0900

    0315

--- spring/exec/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ spring/exec/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,20 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+
+        }
+        this.stockQuantity = restStock;
+    }
+
+
 }

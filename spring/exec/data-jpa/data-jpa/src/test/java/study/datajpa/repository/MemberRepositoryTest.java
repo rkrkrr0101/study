1bd6ddedea577d1984463219fedfe641efd9da23
foko <qkrrudgh0@naver.com>
2023-04-17 16:19:31 +0900

    0417

--- spring/exec/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring/exec/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Transactional
 public class MemberRepositoryTest {
@@ -22,6 +26,34 @@ public class MemberRepositoryTest {
 
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember).isEqualTo(member);
+
+    }
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.setUsername("1111aaa");
+        Member changeFindMember1 = memberRepository.findById(member1.getId()).get();
+        assertThat(changeFindMember1.getUsername()).isEqualTo("1111aaa");
+
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        assertThat(memberRepository.count()).isEqualTo(1);
+
+
 
     }
 }

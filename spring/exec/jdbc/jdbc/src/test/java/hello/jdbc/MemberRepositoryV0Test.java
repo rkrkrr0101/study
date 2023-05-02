b8afca510195ab1560863aa2d4d79bea7f89d622
foko <qkrrudgh0@naver.com>
2023-05-02 15:49:32 +0900

    0502

---
+++ spring/exec/jdbc/jdbc/src/test/java/hello/jdbc/MemberRepositoryV0Test.java
@@ -0,0 +1,38 @@
+package hello.jdbc;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+@Slf4j
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository =new MemberRepositoryV0();
+
+    @Test
+    void crud(){
+        Member member = new Member("memberV3", 10000);
+        repository.save(member);
+
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}",findMember);
+        log.info("member!=findMember ={}",member==findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        repository.update(member.getMemberId(),20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(()->repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+
+
+    }
+
+}

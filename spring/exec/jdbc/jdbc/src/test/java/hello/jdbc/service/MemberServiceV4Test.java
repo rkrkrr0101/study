b68e36000206553b77fcc1eea25cd6b4c6b6276b
foko <qkrrudgh0@naver.com>
2023-05-12 08:00:36 +0900

    0512

--- spring/exec/jdbc/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ spring/exec/jdbc/jdbc/src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -4,6 +4,8 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
+import hello.jdbc.repository.MemberRepositoryV5;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -44,7 +46,7 @@ class MemberServiceV4Test {
 
         @Bean
         MemberRepository memberRepository(){
-            return new MemberRepositoryV4_1(dataSource);
+            return new MemberRepositoryV5(dataSource);
         }
         @Bean
         MemberServiceV4 memberServiceV4(){

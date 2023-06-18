90318cf8a5f0428086fb21d14242a76f51bc2beb
foko <qkrrudgh0@naver.com>
2023-06-18 10:55:36 +0900

    0618

---
+++ spring/exec/aop/aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,39 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy(){
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false);
+
+        MemberService proxy =(MemberService) proxyFactory.getProxy();
+
+        Assertions.assertThrows(ClassCastException.class,()->{
+            MemberServiceImpl Service=(MemberServiceImpl) proxy;
+        });
+    }
+    @Test
+    void cglibProxy(){
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+
+        MemberService proxy =(MemberService) proxyFactory.getProxy();
+
+
+            MemberServiceImpl Service=(MemberServiceImpl) proxy;
+
+    }
+}
---
+++ spring/exec/aop/aop/src/test/java/hello/aop/proxyvs/ProxyDITest.java
@@ -0,0 +1,24 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"})
+public class ProxyDITest {
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberServiceImpl memberServiceImpl;
+
+    @Test
+    void go(){
+        log.info("memberService class={}",memberService.getClass());
+        log.info("memberServiceImpl class={}",memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
+    }
+}
